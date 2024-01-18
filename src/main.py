8ad2e303679750b2e8b844efa81f9e31386f953f
Illuminy00 <calle.m.larsson@gmail.com>
2024-01-18 18:10:04 +0100

    minor changes, still untested

--- src/main.py
+++ src/main.py
@@ -21,6 +21,9 @@ from std_msgs.msg import String
 
 
 #------------------------------------------------------------------------------
+# Global variables
+raw_sensor_data = None
+#------------------------------------------------------------------------------
 # Fuzzy variables
 
 # Define fuzzy input variables (sensors and their range of possible values)
@@ -42,10 +45,10 @@ obstacle_front['yes'] = fuzz.trapmf(obstacle_left.universe, [-1, 0, 0,3, 0,5]) #
 obstacle_right['no'] = fuzz.trapmf(obstacle_left.universe, [0.5, 0.7, 1, 2]) # "Upper", created using the last value outside of range "to the right"
 obstacle_right['yes'] = fuzz.trapmf(obstacle_left.universe, [-1, 0, 0,3, 0,5]) # "Lower", created using the fist value as being outside of range "to the left"
 
-# Control output memberships
-movement['turn_left'] = fuzz.trapmf(movement.universe, [-2, -1, -0.9, -0.4]) # "Lower", created using the fist value as being outside of range "to the left"
+# Control output memberships, use tirangular even at the edges since output has limits
+movement['turn_left'] = fuzz.trimf(movement.universe, [-1, -0.5, 0]) # "Lower", created using the fist value as being outside of range "to the left"
 movement['forward'] = fuzz.trimf(movement.universe, [-0.5, 0, 0.5])
-movement['turn_right'] = fuzz.trapmf(movement.universe, [0.4, 0.9, 1, 2]) # "Upper", created using the last value outside of range "to the right"
+movement['turn_right'] = fuzz.trimf(movement.universe, [0, 0.5, 1]) # "Upper", created using the last value outside of range "to the right"
 
 # Define fuzzy logic rules (always favor left)
 rule1 = ctrl.Rule(obstacle_left['no'] & obstacle_front['no'] & obstacle_right['no'], movement['forward'])
@@ -66,18 +69,24 @@ fuzzy_system = ctrl.ControlSystemSimulation(fuzzy_ctrl)
 #==============================================================================
 # Get sensor readings
 def get_sensor_readings():
+    # Initialize
     rclpy.init()
 
+    # Create subscriber/listener node
     node = rclpy.create_node('sensor_subscriber')
 
-    subscription = node.create_subscription(String, '/scan', lambda msg: node.get_logger().info('I heard: "%s"' % msg.data), 10)
+    # Subscribe to the topic for the lidar/LaserScan
+    subscription = node.create_subscription(String, '/scan', node.listener_callback, 10)
     subscription  # prevent unused variable warning
 
+    # The function executed each time a message is received
+    def listener_callback(node, msg):
+        raw_sensor_data = msg.data
+
+    # Spin until work is complete
     rclpy.spin(node)
 
     # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
     node.destroy_node()
     rclpy.shutdown()
 #==============================================================================
@@ -86,7 +95,7 @@ def get_sensor_readings():
 # Maze solver, decide which movement should be taken
 def movement_choice():
     # Get sensor values (percept)
-    obstacle_left_value, obstacle_front_value, obstacle_right_value = get_sensor_readings()
+    raw_sensor_data
 
     # Provide sensor values to fuzzy system
     fuzzy_system.input['obstacle_left'] = obstacle_left_value
@@ -104,26 +113,35 @@ def movement_choice():
 
 #==============================================================================
 def main():
+    # Initialize
     rclpy.init()
 
-    node = rclpy.create_node('movement_publisher')
-    publisher = node.create_publisher(String, '/cmd_vel', 10)
 
-    msg = String()
+    while(1):
+        # Create publisher node
+        node = rclpy.create_node('movement_publisher')
+        # Publish on command topic
+        publisher = node.create_publisher(String, '/cmd_vel', 10)
 
-    def timer_callback():
-        msg.data = movement_choice()
-        node.get_logger().info('Publishing: "%s"' % msg.data)
-        publisher.publish(msg)
+        msg = String()
 
-    timer_period = 0.5  # seconds
-    timer = node.create_timer(timer_period, timer_callback)
+        # What to do every time timer goes off
+        def timer_callback():
+            msg.data = movement_choice()
+            # Echo what the message contains
+            node.get_logger().info('Publishing: "%s"' % msg.data)
+            # Send message
+            publisher.publish(msg)
 
-    rclpy.spin(node)
+        # Send command every 0.5 seconds
+        timer_period = 0.5  # seconds
+        timer = node.create_timer(timer_period, timer_callback)
+
+        # Spin until work is complete
+        rclpy.spin(node)
+
 
     # Destroy the timer attached to the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
     node.destroy_timer(timer)
     node.destroy_node()
     rclpy.shutdown()
