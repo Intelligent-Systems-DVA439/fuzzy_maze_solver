a9d1413eb8b529709083b577aac0f88d10e408ac
Illuminy00 <calle.m.larsson@gmail.com>
2024-02-19 11:10:22 +0100

    comment update, removed old prints, changed global pathing to now work as a sign changer for fuzzy thus only impacting direction

--- src/lib/robot.py
+++ src/lib/robot.py
@@ -52,7 +52,6 @@ def fuzzy_movement_choice(fuzzy_system, sensor, linear, angular, np_sensor_data)
     fuzzy_system.input['front_sensor'] = (np.min(np.concatenate((np_sensor_data[-20:], np_sensor_data[0:20]), axis=None)) - sensor.min_val)/(sensor.max_val - sensor.min_val)
     # Right value is mean value of a 70 degree cone to the right
     fuzzy_system.input['right_sensor'] = (np.mean(np_sensor_data[-80:-10]) - sensor.min_val)/(sensor.max_val - sensor.min_val)
-    #print("\nL: ",np.mean(np_sensor_data[10:80]),", F: ",np.min(np.concatenate((np_sensor_data[-20:], np_sensor_data[0:20]), axis=None)),", R: ",np.mean(np_sensor_data[-80:-10]),"\n")
 
     # Fuzzy computation
     fuzzy_system.compute()
@@ -68,23 +67,42 @@ def fuzzy_movement_choice(fuzzy_system, sensor, linear, angular, np_sensor_data)
 #==============================================================================
 # Decides which direction to go according to global pathing
 def pathing_direction(state_map, current_state):
+    # NOTE! np array can not be used as a hash key, hence converting it to string
+
     # Check if current state has any neighbors (is not empty)
     if state_map[current_state.tostring()].edges:
         # To begin with, the best state is just the first neighbor
         best_edge = state_map[current_state.tostring()].edges[0]
         for edge in state_map[current_state.tostring()].edges:
+            # Shortest path is minimization problem, thus lower value is better
             if(state_map[edge.end.tostring()].value < state_map[best_edge.end.tostring()].value):
                 best_edge = edge
 
-        # Find which direction should be taken to get to next state
-        direction = best_edge.direction
+        # Find which direction should be taken to get to best state next
+        # Just changes the direction by doing sign change
+        # Clockwise (right) is negative, counter clockwise (left) is positive
+        # Continue right
+        if((best_edge.direction < 0) & (shared_variables.velocity.angular.z < 0)):
+            direction = 1
+        # Turn left
+        elif((best_edge.direction > 0) & (shared_variables.velocity.angular.z < 0)):
+            direction = -1
+        # Turn right
+        elif((best_edge.direction < 0) & (shared_variables.velocity.angular.z > 0)):
+            direction = -1
+        # Continue left
+        elif((best_edge.direction > 0) & (shared_variables.velocity.angular.z > 0)):
+            direction = 1
+
         # Random exploration, chance is reduced the more the maze is explored
+        # 1 run roughly increases the length of state_map by 1300
         random_value = random.randint(0, 100000)
         if (len(state_map) < random_value):
             direction = -direction
+
     # No neighbors, then global pathing can't help and it gives no input
     else:
-        direction = 0
+        direction = 1
 
     return direction
 #==============================================================================
@@ -92,8 +110,6 @@ def pathing_direction(state_map, current_state):
 #==============================================================================
 # Global pathing and mapping
 def global_pathing(np_sensor_data, state_map, path_list, previous_state):
-    # NOTE! np array can not be used as a hash key, hence converting it to string
-
     # Create current state and do mapping of the maze
     current_state = state_mapping(state_map, path_list, previous_state, np_sensor_data)
 
@@ -114,7 +130,6 @@ def movement_choice(fuzzy_system, sensor, linear, angular, state_map, path_list,
 
     # Stuck until sensor has been initialized and sensor values have been received
     while(np.all(np_sensor_data == -1)):
-        print("sensor_value never received")
         np_sensor_data = np.array(shared_variables.raw_sensor_data)
 
     # Set all inf values to max value since average is calculated later
@@ -130,8 +145,8 @@ def movement_choice(fuzzy_system, sensor, linear, angular, state_map, path_list,
     # Final movement choice
     # Linear velocity
     linear_value = fuzzy_linear
-    # Where to turn is based on weighted sum of fuzzy and global pathing
-    angular_value = 1*fuzzy_angular + 1*global_pathing_direction
+    # Turning is based on fuzzy, with input (sign change) from global path on where to turn
+    angular_value = global_pathing_direction*fuzzy_angular
 
     return linear_value, angular_value, current_state
 #==============================================================================
@@ -140,7 +155,6 @@ def movement_choice(fuzzy_system, sensor, linear, angular, state_map, path_list,
 # Controls turtlebot in gazebo
 def robot_control(node_array, fuzzy_system, sensor, linear, angular):
     # Create publisher node
-    # https://www.youtube.com/watch?v=yEwi1__NJrE
     node = rclpy.create_node('movement_publisher')
     # Publish on command topic
     publisher = node.create_publisher(Twist, '/cmd_vel', 10)
@@ -153,7 +167,7 @@ def robot_control(node_array, fuzzy_system, sensor, linear, angular):
     # Variables for global pathing and mapping
     state_map = {}
     path_list = []
-    current_state = np.full((360, 1), -1)
+    current_state = np.full((362, 1), -1)
 
     while(shared_variables.shutdown_flag != True):
         # Decide which linear and angular movement should be taken
