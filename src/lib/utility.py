a908f01ea279b0e2274b37440cf1457e1c62c28a
Illuminy00 <calle.m.larsson@gmail.com>
2024-03-07 12:33:20 +0100

    comment update, changed constant to capital letters, added color coding to map based on value of state and added grid

--- src/lib/utility.py
+++ src/lib/utility.py
@@ -16,6 +16,7 @@
 import time
 import pickle
 import matplotlib.pyplot as plt
+import numpy as np
 
 # rclpy libraries
 import rclpy
@@ -32,20 +33,30 @@ from lib import shared_variables
 def visualize_state_map(state_map):
     x = []
     y = []
+    value = []
 
-    # Add all x and y coordinates to arrays to easily scatter plot them
+    # Add all x and y coordinates and state values to arrays to easily scatter plot them
     for key in state_map:
         x.append(state_map[key].x)
         y.append(state_map[key].y)
+        value.append(state_map[key].value)
 
-    plt.scatter(x, y)
+    # Set clipping on colormap so extreme outliers doesn't ruin it
+    vmin = np.min(np.array(value))
+    vmax = 1*np.average(np.array(value))
 
-    plt.xlim(-shared_variables.maze_boundary_coordinate - 1, shared_variables.maze_boundary_coordinate + 1)
-    plt.ylim(-shared_variables.maze_boundary_coordinate - 1, shared_variables.maze_boundary_coordinate + 1)
+    plt.scatter(x, y, c = value, cmap = 'magma', vmin=vmin, vmax=vmax)
+
+    plt.colorbar(label='State value')
+
+    plt.xlim(-shared_variables.MAZE_BOUNDARY_COORDINATE - 1, shared_variables.MAZE_BOUNDARY_COORDINATE + 1)
+    plt.ylim(-shared_variables.MAZE_BOUNDARY_COORDINATE - 1, shared_variables.MAZE_BOUNDARY_COORDINATE + 1)
 
     plt.xlabel('X-coordinate')
     plt.ylabel('Y-coordinate')
-    plt.title('Plot of all states in state_map')
+    plt.title('Plot of all states in state_map, with color based on state value (low value good)')
+
+    plt.grid(True)
 
     plt.show()
 
@@ -101,10 +112,10 @@ def reset_simulation(node_array):
     # Continously check if turtlebot has made it out of the maze
     while(shared_variables.shutdown_flag == False):
         # Reset simulation once goal is reached
-        if((shared_variables.position.x > shared_variables.maze_boundary_coordinate) | 
-           (shared_variables.position.x < -shared_variables.maze_boundary_coordinate) | 
-           (shared_variables.position.y > shared_variables.maze_boundary_coordinate) | 
-           (shared_variables.position.y < -shared_variables.maze_boundary_coordinate)):
+        if((shared_variables.position.x > shared_variables.MAZE_BOUNDARY_COORDINATE) | 
+           (shared_variables.position.x < -shared_variables.MAZE_BOUNDARY_COORDINATE) | 
+           (shared_variables.position.y > shared_variables.MAZE_BOUNDARY_COORDINATE) | 
+           (shared_variables.position.y < -shared_variables.MAZE_BOUNDARY_COORDINATE)):
             print("Goal reached, reseting")
             time.sleep(1/50)
             reset_world.call_async(request)
@@ -117,7 +128,7 @@ def reset_simulation(node_array):
 #==============================================================================
 # Shutdown function
 def shutdown_function(node_array, executor_array):
-    # Wait for keyboard input q, quit or exit, then initiate shutdown
+    # Wait for keyboard input q, quit, exit or stop, then initiate shutdown
     while(1):
         user_input = input()
         if((user_input.lower() == "q") | (user_input.lower() == "quit") | (user_input.lower() == "exit") | (user_input.lower() == "stop")):
